f8002c9f0498e0010dd1818718f3b16209d6af50
chenzhenyang <czy_1@qq.com>
2019-03-14 20:00:17 +0800

    1. At-moust-once producer and consumer sample

---
+++ src/main/java/com/example/demo/AtMostOnceConsumer.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+/**
+ * 
+
+
+记录的是某个topic，某个分区的偏移量，topic name,partition number;
+
+id，topicname，partition number，offset
+ * @author chenzhenyang
+ *
+ */
+public class AtMostOnceConsumer {
+	public static void main(String[] args) {
+		
+	}
+}
