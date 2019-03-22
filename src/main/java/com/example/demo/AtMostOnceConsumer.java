8cf03a34925f53b18429753c2072942295c22dc7
chenzhenyang <czy_1@qq.com>
2019-03-22 19:42:03 +0800

    1. At Most Once Sample

--- src/main/java/com/example/demo/AtMostOnceConsumer.java
+++ src/main/java/com/example/demo/AtMostOnceConsumer.java
@@ -1,17 +1,39 @@
 package com.example.demo;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 /**
+ * 记录的是某个topic，某个分区的偏移量，topic name,partition number;<br>
+ * id，topicname，partition number，offset<br>
  * 
-
-
-记录的是某个topic，某个分区的偏移量，topic name,partition number;
-
-id，topicname，partition number，offset
  * @author chenzhenyang
  *
  */
 public class AtMostOnceConsumer {
 	public static void main(String[] args) {
-		
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id", "test");
+		props.put("enable.auto.commit", "false");
+		props.put("isolation.level", "read_uncommitted");
+		// props.put("auto.commit.interval.ms", "1000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		@SuppressWarnings("resource")
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		consumer.subscribe(Arrays.asList("test"));
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.of(1L, ChronoUnit.SECONDS));
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.printf("==================offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+			}
+		}
 	}
 }
--- src/main/java/com/example/demo/AtMostOnceProducer.java
+++ src/main/java/com/example/demo/AtMostOnceProducer.java
@@ -1,7 +1,30 @@
 package com.example.demo;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class AtMostOnceProducer {
+	
 	public static void main(String[] args) {
-		
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("acks", "0");// 0或者1
+		props.put("retries", 0);// 关闭重传,只要关闭重传，就只有一次发送消息的机会
+		props.put("enable.idempotence", false);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 0);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		Producer<String, String> producer = new KafkaProducer<>(props);
+		for (int i = 0; i < 100; i++) {
+			producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+		}
+
+		producer.close();
 	}
 }
